85c453d7e68bb3b22a1fcb8f0e861654a1ab5e36
double8ight <sevendays_@hanmail.net>
2014-01-03 18:35:37 +0900

    인디케이터 테스트프로그램입니다.

---
+++ day7_indicatorTest/day7_indicatorTestTests/day7_indicatorTestTests.m
@@ -0,0 +1,34 @@
+//
+//  day7_indicatorTestTests.m
+//  day7_indicatorTestTests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface day7_indicatorTestTests : XCTestCase
+
+@end
+
+@implementation day7_indicatorTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
