c792d3176468be64a24f791105efde77eaa37c7e
double8ight <sevendays_@hanmail.net>
2014-01-03 18:35:47 +0900

    슬라이더테스트 프로그램입니다.

---
+++ day7_sliderTest/day7_sliderTest/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  day7_sliderTest
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+@implementation ViewController
+- (IBAction)progressChanged:(id)sender {
+    
+    UISlider *slider = (UISlider *)sender;
+    _label.text = [NSString stringWithFormat:@"Value : %f", slider.value];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
