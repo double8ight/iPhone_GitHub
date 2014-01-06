3d479408cf283a052e9ab91b3b874b7c80e78ed2
double8ight <sevendays_@hanmail.net>
2014-01-06 16:21:25 +0900

    168페이지 프로토타입 테이블뷰 프로그램입니다.
    테이블뷰 셀을 추가해서 CELL_ID를 넣어주어야하며,
    Custom -> Basic으로 변경해주어야 합니다.

---
+++ day8_168p_prototypeTableView/day8_168p_prototypeTableView/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  day8_168p_prototypeTableView
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+#define CELL_ID @"CELL_ID"
+
+@interface ViewController ()<UITableViewDataSource>
+{
+    NSArray *data;
+}
+
+@end
+
+@implementation ViewController
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
+
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count];
+}
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    data = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i",@"j",@"k",@"l",@"m",@"n",@"o",@"p",@"q",@"r"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
