6360d690c907de906f297309fbb7945ee51f0d56
double8ight <sevendays_@hanmail.net>
2014-01-06 18:37:48 +0900

    178페이지 테이블뷰 셀 삭제입니다.

---
+++ day8_178p_tableViewCellDeleteTest/day8_178p_tableViewCellDeleteTest/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  day8_178p_tableViewCellDeleteTest
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+#define CELL_ID @"CELL_ID"
+
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+
+@end
+
+@implementation ViewController
+{
+    NSMutableArray *data;
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = [[NSMutableArray alloc]initWithObjects:@"a",@"b",@"c",@"d",@"e",@"f", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+// 밀어서 셀 삭제
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //데이터 삭제
+    [data removeObjectAtIndex:indexPath.row ];
+    
+    // 테이블 셀 삭제
+    NSArray *rows = [NSArray arrayWithObject:indexPath];
+    [tableView deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+}
+
+// 셀 개수
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [data count];
+}
+
+// 셀 생성하기, Dynamic Prototypes 방식 사용
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    
+    cell.textLabel.text = [data objectAtIndex:indexPath.row];
+    return cell;
+}
+
+
+
+@end
