27dae24f010859c571f3e6102c31e175d860827f
double8ight <sevendays_@hanmail.net>
2014-01-08 17:42:13 +0900

    수

--- day9_3_3_8_154p_bettingGame/day9_3_3_8_154p_bettingGame/ViewController.m
+++ day9_3_3_8_154p_bettingGame/day9_3_3_8_154p_bettingGame/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#define MAX_NUM 5
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *picker1;
+@property (weak, nonatomic) IBOutlet UIPickerView *picker2;
+@property (weak, nonatomic) IBOutlet UIPickerView *picker3;
+- (IBAction)selectRandom:(id)sender;
 
 @end
 
@@ -26,6 +31,56 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)selectRandom:(id)sender {
+    int r1 = arc4random() % MAX_NUM;
+    [self.picker1 selectRow:r1 inComponent:0 animated:YES];
+    
+    int r2 = arc4random() % MAX_NUM;
+    [self.picker1 selectRow:r2 inComponent:1 animated:YES];
+
+    
+    int r3 = arc4random() % MAX_NUM;
+    [self.picker1 selectRow:r3 inComponent:2 animated:YES];
+
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 3;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return MAX_NUM;
+    
+}
+
+- (CGFloat) pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    return 64;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+{
+    NSString *imagePath = [NSString stringWithFormat:@"fm0%d.jpg", row + 1];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    UIImageView *imageView;
+    
+    if(view == nil)
+    {
+        imageView = [[UIImageView alloc]initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+        
+    }
+    else
+    {
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+    }
+    
+    return imageView;
 }
 
 @end
