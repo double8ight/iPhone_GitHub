86c3234193fa9a87bb032d20b16382c36fa3c593
double8ight <sevendays_@hanmail.net>
2014-01-08 18:35:05 +0900

    수정

--- day9_3_3_8_154p_bettingGame/day9_3_3_8_154p_bettingGame/ViewController.m
+++ day9_3_3_8_154p_bettingGame/day9_3_3_8_154p_bettingGame/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 #define MAX_NUM 5
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+{
+    UIActionSheet *sheet;
+    float height;
+}
 @property (weak, nonatomic) IBOutlet UIPickerView *picker1;
 @property (weak, nonatomic) IBOutlet UIPickerView *picker2;
 @property (weak, nonatomic) IBOutlet UIPickerView *picker3;
@@ -43,6 +47,66 @@
     
     int r3 = arc4random() % MAX_NUM;
     [self.picker1 selectRow:r3 inComponent:2 animated:YES];
+    
+    
+    
+    
+    
+    CGSize viewSize = self.view.bounds.size;
+    
+    if(sheet == nil)
+    {
+        //액션시트 생성
+        sheet = [[UIActionSheet alloc]init];
+        
+        //툴바와 Done 버튼
+        UIToolbar *toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, viewSize.width, 44)];
+        
+        UIBarButtonItem *done = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(handleDone:)];
+        
+        NSArray *items = [NSArray arrayWithObject:done];
+        [toolbar setItems:items];
+        
+        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(20, 50, 0, 0)];
+        label.text = @"배팅금액 입력";
+        label.textColor = [ UIColor blackColor];
+        label.backgroundColor = [UIColor clearColor];
+        [label sizeToFit];
+        [sheet addSubview:label];
+        
+        UITextField *textField = [[UITextField alloc]initWithFrame:CGRectMake(130, 50, 171, 31)];
+        
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+        textField.keyboardType = UIKeyboardTypeNumberPad;
+        //textField.delegate = self;
+        
+        [sheet addSubview:textField];
+        
+        
+        
+        
+        // 시트에 추가
+        [sheet addSubview:toolbar];
+        
+        //시트 나타나기
+        [sheet showInView:self.view];
+        
+        
+        // 액션시트크기와 위치계산용
+        height = toolbar.frame.size.height + textField.frame.size.height + 250;
+        
+        
+    }
+    
+    
+    [sheet showInView:self.view];
+    sheet.frame = CGRectMake(0, viewSize.height - height, viewSize.width, height);
+
+}
+
+- (void)handleDone:(id)sender
+{
+    [sheet dismissWithClickedButtonIndex:0 animated:YES];
 
 }
 
